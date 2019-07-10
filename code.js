0c26a3e7c982be2d63c34b85e2c423b5f5b51e45
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2019-07-10 23:18:17 +0100

    Initial sketch of binary search

--- code.js
+++ code.js
@@ -11,33 +11,51 @@ var inputs = readline().split(' ');
 const X0 = parseInt(inputs[0]);
 const Y0 = parseInt(inputs[1]);
 
-var newX= 0;
-var newY = 1;
+const maxY = H -1;
+
+var newX = X0;
+var newY = Y0;
+
+var lowY = 0;
+var highY = 0;
 
 // game loop
 while (true) {
     const bombDir = readline(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-    
-    if( bombDir.includes("D"))
-    {
-        newY = parseInt(H - Y0 / 2);
-        console.log(`0 ${newY}`);
-    }
-    
-    if(bombDir.includes("R"))
-    {
-        newX = parseInt(W - X0 / 2);
-        console.log(`${newX} ${newY}`);
-    }
-    
-    else
-    {
-        console.log('0 0');
-    }
+    console.error(bombDir);
+
+    setNewY(bombDir);
+
+    if (bombDir.includes("R"))
+        newX = W - 1;
+
+    if (bombDir.includes("L"))
+        newX = parseInt(newX / 2) - 1;
+
+    console.log(`${newX} ${newY}`);
+
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
 
 
     // the location of the next window Batman should jump to.
+
+}
+
+function setNewY(bombDir) {
+    if (bombDir.includes("D"))
+    {
+        lowY = newY;
+        newY = newY + parseInt((maxY - newY)/2) + 1        
+    }
     
+    if (bombDir.includes("U"))
+    {
+        newY = newY - parseInt((newY - lowY) / 2);
+        highY = newY;
+        
+    }
+
+    if (newY >= H)
+        newY = H -1;
 }
